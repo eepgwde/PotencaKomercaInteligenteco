484155ade7cd52dfb268cc6e7093133308809605
Walter Eaves <walti@walti.mooo.com>
2021-10-01 16:14:16 +0200

    -

---
+++ lab1.ps1
@@ -0,0 +1,11 @@
+﻿<#
+   Utility methods to prepare data for tutorial
+
+   Cygwin bash from this directory
+
+   powershell -ExecutionPolicy Unrestricted -command ./lab1.ps1
+
+#>
+
+
+Expand-Archive -Path .\msoft\Lab1\AccessDatabasePowerBI.zip -DestinationPath .\cache
